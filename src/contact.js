927a3d04e7e3201578825ad0be6a7f7abdf35f44
noam-ackerman <developer.noam@gmail.com>
2021-09-23 19:06:57 +0200

    adding js to contact form to prevent submission when fields are empty

---
+++ src/contact.js
@@ -0,0 +1,19 @@
+let sendBtn = document.querySelector(".sendBtn");
+let errorNote = document.querySelector(".error");
+
+sendBtn.addEventListener("click", Validate);
+
+function Validate(e) {
+  var inputs = [
+    document.getElementById("email"),
+    document.getElementById("message"),
+  ];
+
+  inputs.map((input) => {
+    if (input.value == "") {
+      errorNote.innerHTML = "Please complete all fields!";
+      e.preventDefault();
+      return false;
+    }
+  });
+}
