78c3e035cf54893ffdf07541bf38d250e5babdd2
Noam Ackerman <developer.noam@gmail.com>
2023-07-12 13:50:16 +0300

    adding a page loader before all DOM content and images in page are loaded

---
+++ src/pageLoading.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const images = Array.from(document.images);
+  const background = new Image();
+  background.src = "../img/background.jpg";
+  images.push(background);
+  Promise.all(
+    images.map((img) => {
+      if (img.complete) return Promise.resolve(true);
+      return new Promise((resolve) => {
+        img.addEventListener("load", () => resolve(true));
+        img.addEventListener("error", () => resolve(false));
+      });
+    })
+  ).then(() => {
+    setTimeout(() => {
+      document.querySelector(".spinner-wrapper").remove();
+      document.querySelector(".container").style.opacity = "1";
+    }, 300);
+  });
+});
